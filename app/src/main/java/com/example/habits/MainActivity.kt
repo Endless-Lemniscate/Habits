db3bd5a4e93037381009c0d40c05bfded45e7b3f
Endless-Lemniscate <endless-lemniscate@yandex.ru>
2020-10-17 23:19:04 +0300

    manifest

--- app/src/main/java/com/example/habits/MainActivity.kt
+++ app/src/main/java/com/example/habits/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.habits
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+
+
+    override fun onStart() {
+        super.onStart()
+        textView.text = "Hello world"
     }
 }
