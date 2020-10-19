509d26aa28f80367fc5945e580b2675b94896623
Endless-Lemniscate <endless-lemniscate@yandex.ru>
2020-10-19 20:54:33 +0300

    Delete on swipe added

--- app/src/main/java/com/example/habits/MainActivity.kt
+++ app/src/main/java/com/example/habits/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.habits
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.habits.models.Habit
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
     private lateinit var viewManager: RecyclerView.LayoutManager
     private lateinit var viewAdapter: RecyclerView.Adapter<*>
     private val data: MutableList<Habit> = ArrayList()
@@ -19,7 +21,8 @@ class MainActivity : AppCompatActivity() {
 
         viewManager = LinearLayoutManager(this)
         viewAdapter = HabitsRecyclerAdapter(data)
-        recycler_view.apply {
+        recyclerView = recycler_view
+        recyclerView.apply {
             setHasFixedSize(true)
             layoutManager = viewManager
             adapter = viewAdapter
@@ -29,6 +32,8 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, EditActivity::class.java)
             startActivity(intent)
         }
+
+        setRecyclerViewItemTouchListener()
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -59,4 +64,29 @@ class MainActivity : AppCompatActivity() {
             viewAdapter.notifyDataSetChanged()
         }
     }
+
+    private fun setRecyclerViewItemTouchListener() {
+        val itemTouchCallback =
+            object :
+                ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    viewHolder1: RecyclerView.ViewHolder
+                ): Boolean {
+
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, swipeDir: Int) {
+                val position = viewHolder.adapterPosition
+                data.removeAt(position)
+                recyclerView.adapter!!.notifyItemRemoved(position)
+            }
+        }
+
+        val itemTouchHelper = ItemTouchHelper(itemTouchCallback)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
 }
